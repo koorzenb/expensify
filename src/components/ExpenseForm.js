c7e8e035a3b0e2d71d1f2b0c5f6a6e15287c3ddc
Barend Koorzen <koorzenb@gmail.com>
2025-01-22 11:51:26 -0400

    Mocking libraries with Jest

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import moment from 'moment';
 import { SingleDatePicker } from 'react-dates';
-import 'react-dates/lib/css/_datepicker.css';
 
 export default class ExpenseForm extends React.Component {
   constructor(props) {
@@ -13,7 +12,7 @@ export default class ExpenseForm extends React.Component {
       amount: props.expense ? (props.expense.amount / 100).toString() : '',
       createdAt: props.expense ? moment(props.expense.createdAt) : moment(),
       calendarFocused: false,
-      error: ''
+      error: '',
     };
   }
   onDescriptionChange = (e) => {
@@ -50,7 +49,7 @@ export default class ExpenseForm extends React.Component {
         description: this.state.description,
         amount: parseFloat(this.state.amount, 10) * 100,
         createdAt: this.state.createdAt.valueOf(),
-        note: this.state.note
+        note: this.state.note,
       });
     }
   };
@@ -60,15 +59,15 @@ export default class ExpenseForm extends React.Component {
         {this.state.error && <p>{this.state.error}</p>}
         <form onSubmit={this.onSubmit}>
           <input
-            type="text"
-            placeholder="Description"
+            type='text'
+            placeholder='Description'
             autoFocus
             value={this.state.description}
             onChange={this.onDescriptionChange}
           />
           <input
-            type="text"
-            placeholder="Amount"
+            type='text'
+            placeholder='Amount'
             value={this.state.amount}
             onChange={this.onAmountChange}
           />
@@ -81,14 +80,12 @@ export default class ExpenseForm extends React.Component {
             isOutsideRange={() => false}
           />
           <textarea
-            placeholder="Add a note for your expense (optional)"
+            placeholder='Add a note for your expense (optional)'
             value={this.state.note}
-            onChange={this.onNoteChange}
-          >
-          </textarea>
+            onChange={this.onNoteChange}></textarea>
           <button>Add Expense</button>
         </form>
       </div>
-    )
+    );
   }
 }
