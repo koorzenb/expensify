395c5c720cd7bf4b822c3cd15d742840acdd5d24
Barend Koorzen <koorzenb@gmail.com>
2025-03-11 13:27:32 -0300

    Updated filename extensions

--- src/selectors/expenses-total.js
+++ src/selectors/expenses-total.js
@@ -1,3 +1,5 @@
 export default (expenses) => {
-  return expenses.map((expense) => expense.amount).reduce((a, b) => a + b, 0);
+  return expenses
+      .map((expense) => expense.amount)
+      .reduce((sum, value) => sum + value, 0);
 };
